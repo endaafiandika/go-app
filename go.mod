8b5eddd87d7b20893fdba6be1068f500f13ad125
teamborneo <endaafiandikaa@gmail.com>
2021-01-27 13:24:31 +0700

    add dockerfile

--- go.mod
+++ go.mod
@@ -1,8 +1,10 @@
-module github.com/endaafiandika/go-app
+module github.com/go-app
 
 go 1.15
 
 require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/gorilla/mux v1.8.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -25,7 +25,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// Create Server and Route Handlers
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", handler)
@@ -37,19 +36,17 @@ func main() {
 		WriteTimeout: 10 * time.Second,
 	}
 
-	// Configure Logging
 	LOG_FILE_LOCATION := os.Getenv("LOG_FILE_LOCATION")
 	if LOG_FILE_LOCATION != "" {
 		log.SetOutput(&lumberjack.Logger{
 			Filename:   LOG_FILE_LOCATION,
-			MaxSize:    500, // megabytes
+			MaxSize:    500,
 			MaxBackups: 3,
-			MaxAge:     28,   //days
-			Compress:   true, // disabled by default
+			MaxAge:     28,
+			Compress:   true,
 		})
 	}
 
-	// Start Server
 	go func() {
 		log.Println("Starting Server")
 		if err := srv.ListenAndServe(); err != nil {
@@ -57,7 +54,6 @@ func main() {
 		}
 	}()
 
-	// Graceful Shutdown
 	waitForShutdown(srv)
 }
 
@@ -65,10 +61,8 @@ func waitForShutdown(srv *http.Server) {
 	interruptChan := make(chan os.Signal, 1)
 	signal.Notify(interruptChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
-	// Block until we receive our signal.
 	<-interruptChan
 
-	// Create a deadline to wait for.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 	srv.Shutdown(ctx)
